03c3c5117e5de8293ca7ae7d28cc41cd5d3f5677
Taaha Chaudhry <t44h413@gmail.com>
2014-05-27 19:11:56 +0000

    Inital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CitySpots::Application.config.secret_key_base = 'dc6e30cf7cbaf15c8acd0e6e25b77920b9a0a615a2e6786d9dd2672adcecd3ea8e757b11a4e2ab6007455545d4bf1b22cf09b30710b07fb1e2cb7e2a737a872d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CitySpots::Application.config.session_store :cookie_store, key: '_CitySpots_session'
