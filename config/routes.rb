38d8db10bcf45874c67802e9a317c79ed270e034
Taaha Chaudhry <t44h413@gmail.com>
2014-05-27 20:05:36 +0000

    Updated name from places to spots and added a spot to database and displayed on places#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 CitySpots::Application.routes.draw do
-  root 'places#index'
+  root 'spots#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
