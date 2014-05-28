c6f24436e0950ae56379afdea71a79563261bd8e
Taaha Chaudhry <t44h413@gmail.com>
2014-05-28 02:34:52 +0000

    Added create method to save new places to database from form

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -6,4 +6,15 @@ class SpotsController < ApplicationController
   def new
     @spot = Spot.new
   end
+
+  def create
+    Spot.create(spot_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def spot_params
+    params.require(:spot).permit(:name, :description, :address)
+  end
 end
