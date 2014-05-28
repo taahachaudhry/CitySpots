6c669b5de6a9570813afc5ab20f063e4e3c99067
Taaha Chaudhry <t44h413@gmail.com>
2014-05-28 02:21:27 +0000

    Added a simple_form to save new spots

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 CitySpots::Application.routes.draw do
+
   root 'spots#index'
+
+  resources :spots
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
