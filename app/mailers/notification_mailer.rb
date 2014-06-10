21a0f6e9352e5385e038ab9b06f9b57620d57ab1
Taaha Chaudhry <t44h413@gmail.com>
2014-06-10 21:15:39 +0000

    Adjusted the email notif mailer

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,8 +1,11 @@
 class NotificationMailer < ActionMailer::Base
   default from: "no-reply@thecityspots.com"
 
-  def comment_added
-    mail(to: "taahac@taahac.me",
-         subject: "A comment has been added to your place")
+  def comment_added(comment)
+    @spot = comment.spot
+    @spot_owner = @spot.user
+
+    mail(to: @spot_owner.email,
+         subject: "A comment has been added to #{@spot.name}")
   end
 end
